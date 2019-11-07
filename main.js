f4b2514d7c40532190581fd516e94b688b827b24
ArmFrPl <ehakobkekhvyan7@gmail.com>
2019-11-07 12:41:54 +0400

    updated main.js

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-
+const reader = require('readline-sync');
 const area = require('./area.js');
 
-const result = area.rectArea(6, 2);
+console.log("Supported Shapes are:");
+console.log("1: Circle, 2: Rectangle");
+console.log("3: Square, 4: Triangle");
 
-console.log("Result is = " + result)
+const opt = reader.questionInt("What is your choice of shape? ");
+let result;
+
+switch (opt) {
+  case 1:
+    const radius = reader.questionInt("Radius =  ");
+    result = area.circleArea(radius);
+    break;
+  case 2:
+    const width = reader.questionInt("Width =  ");
+    const length = reader.questionInt("Length =  ");
+    result = area.rectArea(length, width);
+    break;
+  case 3:
+    const side = reader.questionInt("Side =  ");
+    result = area.squareArea(side);
+    break;
+  case 4:
+    const base = reader.questionInt("Base =  ");
+    const height = reader.questionInt("Height =  ");
+    result = area.triangleArea(height, base);
+    break;
+  default:
+    console.log("Type is not supported!");
+}
+
+if(result){
+  console.log("Result = " + result)
+}
